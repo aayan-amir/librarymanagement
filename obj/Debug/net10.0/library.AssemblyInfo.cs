dc521583e64a5e1a8cf8c5a73653eb00dd106920
Aayan Amir <aaaaayan104@gmail.com>
2026-06-18 11:59:30 +0500

    vs

--- obj/Debug/net10.0/library.AssemblyInfo.cs
+++ obj/Debug/net10.0/library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69c0aa2ee24c2a4c7e7384f0448921a59081c866")]
 [assembly: System.Reflection.AssemblyProductAttribute("library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
